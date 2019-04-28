ae5ae041996108323a9afc9f54c8f7c6e5c11140
yiwei <wangyiwei11259@gmail.com>
2019-04-28 17:53:29 +0800

    Implement merge-sort with double-linked list

---
+++ examples/merge-sort.c
@@ -0,0 +1,99 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/list.h"
+
+#include "../private/common.h"
+
+static uint16_t values[256];
+
+static void list_merge(struct list_head *list_left,
+                       struct list_head *list_right,
+                       struct list_head *head)
+{
+    while (!list_empty(list_left) && !list_empty(list_right)) {
+        struct listitem *l_item =
+            list_entry(list_left->next, struct listitem, list);
+        struct listitem *r_item =
+            list_entry(list_right->next, struct listitem, list);
+
+        if (cmpint(&l_item->i, &r_item->i) < 0) {
+            list_move_tail(&l_item->list, head);
+        } else {
+            list_move_tail(&r_item->list, head);
+        }
+    }
+
+    if (!list_empty(list_left)) {
+        list_splice_tail(list_left, head);
+    }
+
+    if (!list_empty(list_right)) {
+        list_splice_tail(list_right, head);
+    }
+}
+
+static void list_mergesort(struct list_head *head)
+{
+    if (list_empty(head) || list_is_singular(head))
+        return;
+
+    struct list_head list_left, list_right;
+    struct listitem *item = NULL, *is = NULL;
+
+    INIT_LIST_HEAD(&list_left);
+    INIT_LIST_HEAD(&list_right);
+
+    short add_to_left = 1;
+    list_for_each_entry_safe (item, is, head, list) {
+        if (add_to_left) {
+            list_move_tail(&item->list, &list_left);
+            add_to_left = 0;
+        } else {
+            list_move_tail(&item->list, &list_right);
+            add_to_left = 1;
+        }
+    }
+
+    list_mergesort(&list_left);
+    list_mergesort(&list_right);
+    list_merge(&list_left, &list_right, head);
+}
+
+int main(void)
+{
+    struct list_head testlist;
+    struct listitem *item, *is = NULL;
+    size_t i;
+
+    random_shuffle_array(values, (uint16_t) ARRAY_SIZE(values));
+
+    INIT_LIST_HEAD(&testlist);
+
+    assert(list_empty(&testlist));
+
+    for (i = 0; i < ARRAY_SIZE(values); i++) {
+        item = (struct listitem *) malloc(sizeof(*item));
+        assert(item);
+        item->i = values[i];
+        list_add_tail(&item->list, &testlist);
+    }
+
+    assert(!list_empty(&testlist));
+
+    qsort(values, ARRAY_SIZE(values), sizeof(values[0]), cmpint);
+    list_mergesort(&testlist);
+
+    i = 0;
+    list_for_each_entry_safe (item, is, &testlist, list) {
+        assert(item->i == values[i]);
+        list_del(&item->list);
+        free(item);
+        i++;
+    }
+
+    assert(i == ARRAY_SIZE(values));
+    assert(list_empty(&testlist));
+
+    return 0;
+}
